802627e16aeaf95ce3576606c3e45552dcb4f368
Dylan Reuter <djreuter45@gmail.com>
2017-07-14 11:23:58 -0600

    added conceptual model

--- index.php
+++ index.php
@@ -17,14 +17,14 @@
 			site before, so it will be a new experience for her. Based on her familiarity with purchasing, she
 			is confident about her ability to post her items and does not anticipate any problems.</p>
 		<p><strong>Technology:</strong> Beth is devoted to Apple and has the latest iPhone 7, iPad Pro, a Macbook Air,
-			and a 21.5" iMac. She is comfortable in using them, but does not know the entire scope of what they can do.</p>
-		<p><strong>Attitudes & Behaviors:</strong> Beth is very busy during the day taking care of her two kids. She
+			and a 21.5" iMac. She is comfortable using them, but does not know the entire scope of what they can do.</p>
+		<p><strong>Attitudes &amp; Behaviors:</strong> Beth is very busy during the day taking care of her two kids. She
 			often gets up early in the morning before her kids in order to have some quiet time to herself and drink
 			coffee. This is when she will browse Etsy the most. Throughout the rest of the day, she will browse Etsy
 			off and on while her kids are napping or otherwise entertained. However her browsing sessions are
 			often limited to 5 - 10 minutes. At night after her kids go to bed is when Beth will spend an hour working
 			on her crafts as a way to relax before going to bed.</p>
-		<p><strong>Frustrations & Needs:</strong> Beth is pretty familiar with the motions she goes through regularly
+		<p><strong>Frustrations &amp; Needs:</strong> Beth is pretty familiar with the motions she goes through regularly
 			on all of her devices, as well and the purchasing process on Etsy. However she sometimes requires assistance
 			if she is performing a new task on her iPhone or computer. Beth will attempt to figure it out on her own for
 			about 3 to 5 minutes before getting frustrated and quiting, only to ask her friend or husband about it later
@@ -40,11 +40,10 @@
 			even took pictures of her items the night before and has them in a folder on her desktop ready to be
 			uploaded to her posts. She knows she has about 45 min before her kids usually wake up and she wants to hurry
 			and get it done.</p>
-		<p><strong>Interaction Flow:</strong> On Etsy homepage, user clicks on &quot;Sell on Etsy&quot;. The browser then
-			goes to
-			a new page with a button in the center that says &quot;Open your Etsy shop&quot;. Below that on the page are
-			highlights of selling on Etsy, such as low fees, powerful tools, and support and education, which all have a
-			&quot;learn more&quot; button on the bottom to read about each one.</p>
+		<p><strong>Interaction Flow:</strong> On Etsy homepage, user clicks on &quot;Sell on Etsy&quot;. The browser
+			then goes to a new page with a button in the center that says &quot;Open your Etsy shop&quot;. Below that on
+			the page are highlights of selling on Etsy, such as low fees, powerful tools, and support and education,
+			which all have a &quot;learn more&quot; button on the bottom to read about each one.</p>
 		<p>After the user clicks on &quot;Open your Etsy shop&quot; the browser has a pop up on the same screen that
 			asks the user to either create an account or log in.</p>
 		<p>After the user logs in, the first page shows &quot;Shop Preferences&quot; where the user selects the shop's
@@ -79,9 +78,31 @@
 			information fields populate on the screen asking:</p>
 		<dl>
 			<dt>Where should we deposit your funds?</dt>
-			<dd>Asks for bank account information such as bank name, name on account, account number, and BSB code</dd>
+			<dd>- Asks for bank account information such as bank name, name on account, account number, and BSB code</dd>
 			<dt>Tell us a little about yourself</dt>
-			<dd>Country of residence, first name, last name, date of birth, and home address</dd>
+			<dd>- Country of residence, first name, last name, date of birth, and home address</dd>
 		</dl>
+		<p>The last section the user is brought to after clicking on &quot;Save and Continue&quot; is &quot;Set up
+			Billing&quot;</p>
+		<p>After these steps have been completed, the item(s) will be successfully posted for sale.</p>
+		<p><strong>Conceptual Model</strong></p>
+		<p>Profile (1-to-1)</p> <!-- one user can have one profile/account -->
+		<ul>
+			<li>profileId(primary key)</li>
+			<li>profileEmail</li>
+			<li>profilePass</li>
+			<li>profileContact</li>
+		</ul>
+		<p>Item (1-to-n)</p> <!-- one profile can have many items for sale -->
+		<ul>
+			<li>itemId(primary key)</li>
+			<li>itemProfileId(foreign key)</li>
+			<li>itemInfo</li>
+		</ul>
+		<p>Favorites(m-to-n)</p> <!-- many profiles can favorite many items -->
+		<ul>
+			<li>favUserId(foreign key)</li>
+			<li>favItemId(foreign key)</li>
+		</ul>
 	</body>
 </html>
