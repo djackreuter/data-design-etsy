412beed5230e19b614970731c1f6f7537d18aa5e
Dylan Reuter <djreuter45@gmail.com>
2017-07-13 19:46:42 -0600

    corrected html style in compliance with style guide

--- index.php
+++ index.php
@@ -7,20 +7,14 @@
 	<body>
 		<h1>Data Design Etsy</h1>
 		<h2>Persona</h2>
-		<p>
-			<strong>Name:</strong> Beth Macbeth
-		</p>
-		<p>
-			<strong>Age:</strong> 31
-		</p>
-		<p>
-			<strong>Profession:</strong> Beth is a stay at home mom with a 2 year old daughter and a newborn son. She is
+		<p><strong>Name:</strong> Beth Macbeth</p>
+		<p><strong>Age:</strong> 31</p>
+		<p><strong>Profession:</strong> Beth is a stay at home mom with a 2 year old daughter and a newborn son. She is
 			a long time user of Etsy and really enjoys the crafts and trinkets that other people have made. Beth has
 			dabbled in making her own crafts before and is getting ready to put some of them up for sale for the first
 			time. While she is very familiar with the purchasing process on Etsy, she has never sold anything on the
 			site before, so it will be a new experience for her. Based on her familiarity with purchasing, she
-			is confident about her ability to post her items and does not anticipate any problems.
-		</p>
+			is confident about her ability to post her items and does not anticipate any problems.</p>
 
 	</body>
 </html>
