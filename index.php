0af7c6dba91c9df3c4a501db1624e21bc2e8db5e
Dylan Reuter <djreuter45@gmail.com>
2017-07-13 20:36:01 -0600

    added goals and use case completing persona

--- index.php
+++ index.php
@@ -11,23 +11,34 @@
 		<p><strong>Age:</strong> 31</p>
 		<p><strong>Profession:</strong> Beth is a stay at home mom with a 2 year old daughter and a newborn son. She is
 			a long time user of Etsy and really enjoys the crafts and trinkets that other people have made. Beth has
-			dabbled in making her own crafts before and is getting ready to put some of them up for sale for the first
+			dabbled in making her own crafts before and is getting ready to put 3 of them up for sale for the first
 			time. While she is very familiar with the purchasing process on Etsy, she has never sold anything on the
 			site before, so it will be a new experience for her. Based on her familiarity with purchasing, she
 			is confident about her ability to post her items and does not anticipate any problems.</p>
 		<p><strong>Technology:</strong> Beth is devoted to Apple and has the latest iPhone 7, iPad Pro, a Macbook Air,
-			and a 21.5" iMac.</p>
+			and a 21.5" iMac. She is comfortable in using them, but does not know the entire scope of what they can do.</p>
 		<p><strong>Attitudes & Behaviors:</strong> Beth is very busy during the day taking care of her two kids. She
 			often gets up early in the morning before her kids in order to have some quiet time to herself and drink
 			coffee. This is when she will browse Etsy the most. Throughout the rest of the day, she will browse Etsy
-			off and on while her kids are either napping or otherwise entertained. However her browsing sessions are
+			off and on while her kids are napping or otherwise entertained. However her browsing sessions are
 			often limited to 5 - 10 minutes. At night after her kids go to bed is when Beth will spend an hour working
 			on her crafts as a way to relax before going to bed.</p>
 		<p><strong>Frustrations & Needs:</strong> Beth is pretty familiar with the motions she goes through regularly
 			on all of her devices, as well and the purchasing process on Etsy. However she sometimes requires assistance
 			if she is performing a new task on her iPhone or computer. Beth will attempt to figure it out on her own for
 			about 3 to 5 minutes before getting frustrated and quiting, only to ask her friend or husband about it later
-			on if she remembers.</p>
+			on if she remembers. Beth wants to be able to post her crafts for sale as easily as she makes purchases.
+			She will be doing this in the morning and will have limited time before her children get up and she has to
+			start her day.</p>
+		<p><strong>Goals:</strong> Beth wants to sell her crafts that she makes on Etsy and wants other people to enjoy
+			them just as much as she enjoys her purchases from others. Beth is sensitive to everything going smoothly
+			for posting her items, especially sense this is her first time. She knows that she doesn't have a lot of
+			time to do this and will not have the time or patience for unexpected problems.</p>
+		<p><strong>Use Case:</strong> Beth is up early as usual on a Tuesday morning with her coffee in hand and all
+			set to post her items. She has her Macbook open to Etsy and is all logged in and ready to go. She
+			even took pictures of her items the night before and has them in a folder on her desktop ready to be
+			uploaded to her posts. She knows she has about 45 min before her kids usually wake up and she wants to hurry
+			and get it done.</p>
 
 	</body>
 </html>
