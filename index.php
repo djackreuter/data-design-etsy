fe95aa2613a265d3d44c52ad6ac49fc442cf88ab
Dylan Reuter <djreuter45@gmail.com>
2017-07-13 20:11:15 -0600

    added tech, attitudes & behaviors, and frustrations and needs to the persona

--- index.php
+++ index.php
@@ -15,6 +15,19 @@
 			time. While she is very familiar with the purchasing process on Etsy, she has never sold anything on the
 			site before, so it will be a new experience for her. Based on her familiarity with purchasing, she
 			is confident about her ability to post her items and does not anticipate any problems.</p>
+		<p><strong>Technology:</strong> Beth is devoted to Apple and has the latest iPhone 7, iPad Pro, a Macbook Air,
+			and a 21.5" iMac.</p>
+		<p><strong>Attitudes & Behaviors:</strong> Beth is very busy during the day taking care of her two kids. She
+			often gets up early in the morning before her kids in order to have some quiet time to herself and drink
+			coffee. This is when she will browse Etsy the most. Throughout the rest of the day, she will browse Etsy
+			off and on while her kids are either napping or otherwise entertained. However her browsing sessions are
+			often limited to 5 - 10 minutes. At night after her kids go to bed is when Beth will spend an hour working
+			on her crafts as a way to relax before going to bed.</p>
+		<p><strong>Frustrations & Needs:</strong> Beth is pretty familiar with the motions she goes through regularly
+			on all of her devices, as well and the purchasing process on Etsy. However she sometimes requires assistance
+			if she is performing a new task on her iPhone or computer. Beth will attempt to figure it out on her own for
+			about 3 to 5 minutes before getting frustrated and quiting, only to ask her friend or husband about it later
+			on if she remembers.</p>
 
 	</body>
 </html>
