0dc0e294e776b05e4d930336b2a3ffd157f695dc
Dylan Reuter <djreuter45@gmail.com>
2017-07-19 19:59:27 -0600

    adding more data to table

--- sql/dml-example.sql
+++ sql/dml-example.sql
@@ -1,18 +1,42 @@
 -- insert data into the created tables
-INSERT INTO profile(profileId, profileEmail, profileSalt, profileHash, profileContact)
-	VALUES(1, 'user1@user.com', 'password', 'password', 'user contact');
-INSERT INTO profile(profileId, profileEmail, profileSalt, profileHash, profileContact)
-	Values(2, 'user2@user.com', 'usr2pass', 'usr2pass', 'user2 contact info');
-INSERT INTO profile(profileId, profileEmail, profileHash, profileSalt, profileContact)
-	VALUES(3, 'user3@user.com', 'usr3pass', 'usr3pass', 'user 3 contact info');
-INSERT INTO profile(profileId, profileEmail, profileHash, profileSalt, profileContact)
-	VALUES(4, 'user4@user.com', 'usr4pass', 'usr4pass', 'user 4 contact info');
+-- profile
+INSERT INTO profile(profileEmail, profileSalt, profileHash, profileContact)
+	VALUES('user1@user.com', 'password', 'password', 'user contact');
+INSERT INTO profile(profileEmail, profileSalt, profileHash, profileContact)
+	Values('us2r@us2r.com', 'usr2pass', 'usr2pass', 'user2 contact info');
+INSERT INTO profile(profileEmail, profileHash, profileSalt, profileContact)
+	VALUES('us3r@us3r.com', 'usr3pass', 'usr3pass', 'user 3 contact info');
+INSERT INTO profile(profileEmail, profileHash, profileSalt, profileContact)
+	VALUES('user4@user4.com', 'usr4pass', 'usr4pass', 'user 4 contact info');
 
--- grab the primary keys from the insert statements
+-- grab the primary key from the insert statements
+-- or? SELECT * FROM table;
+SELECT profileId FROM profile;
+
+-- items
+INSERT INTO item(itemProfileId, itemInfo, itemDate)
+	VALUES(1, 'item info', NOW());
+INSERT INTO item(itemProfileId, itemInfo, itemDate)
+	VALUES(2, 'item info', NOW());
+INSERT INTO item(itemProfileId, itemInfo, itemDate)
+	VALUES(1, 'item info', NOW());
+INSERT INTO item(itemProfileId, itemInfo, itemDate)
+	VALUES(2, 'Item Info', NOW());
+
+-- favorites
+-- select both foreign keys
+
+SELECT itemId from item;
+
+INSERT INTO favorites()
 
 
-INSERT INTO item(itemId, itemProfileId, itemInfo)
-	VALUES(1, 1, 'item 1 user 1 info');
+
+
+
+
+
+
 
 
 
