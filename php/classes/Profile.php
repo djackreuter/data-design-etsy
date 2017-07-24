cb87b33f60a59ed492ce8ea6e72e8d890d27a393
Dylan Reuter <djreuter45@gmail.com>
2017-07-24 16:00:02 -0600

    added accessor and mutator methods for profileId and profileEmail

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -39,5 +39,53 @@ class Profile {
 	 * @var $profileActivationToken
 	 **/
 	private $profileActivationToken;
+	/**
+	 * accessor method for profile id
+	 * @return int|null value of $profileId
+	 **/
+	public function getProfileId() : int {
+		return($this->profileId);
+	}
+	/**
+	 * mutator method for profile id
+	 *
+	 * @param int|null $newProfileId new value of profile id
+	 * @throws rangeException if $newProfileId is not positive
+	 * @throws typeError if $newProfileId is not an integer
+	 **/
+	public function setProfileId(?int $newProfileId) : void {
+		// if tweet id is null, immediately return it
+		if($newProfileId === null) {
+			$this->profileId = null;
+			return;
+		}
+	}
+	/**
+	 * accessor method for the profile email
+	 * @return string value of $profileEmail
+	 **/
+	public function getProfileEmail() : string {
+		return($this->profileEmail);
+	}
+	/**
+	 * mutator method for profile email
+	 * @param string $newProfileEmail new value of profile email
+	 * @throws RangeException if $newProfileEmail is > 128 characters
+	 * @throws unexpectedValueException if $newProfileId is not a string
+	 **/
+	public function setProfileEmail(string $newProfileEmail) : void {
+		// verify email is secure
+		$newProfileEmail = trim($newProfileEmail);
+		$newProfileEmail = filter_var($newProfileEmail, FILTER_SANITIZE_EMAIL);
+		if(empty($newProfileEmail) === true) {
+			throw(new UnexpectedValueException("Please enter a valid email"));
+		}
+		// make sure email is not too long for the database
+		if(strlen($newProfileEmail) > 128) {
+			throw(new RangeException("Email is too long"));
+		}
+		// store profile email
+		$this->profileEmail = $newProfileEmail;
+	}
 
 }
